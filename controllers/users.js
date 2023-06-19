4ee390f25eaff8f40c689fc7df777cd9780133d0
Rupesh Kumar Dash <rupeshkumardash@Rupeshs-MacBook-Pro.local>
2023-06-20 00:14:33 +0530

    first commit

---
+++ controllers/users.js
@@ -0,0 +1,67 @@
+import User from "../models/User.js";
+
+//READ
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.id;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getUserFriend = async (req, res) => {
+  try {
+    const { id } = req.id;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friendList.map((id) => {
+        User.findById(id);
+      })
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+//UPDATE
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = User.findById(id);
+    const friend = User.findById(friendId);
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filer((id)=> id !=id)
+    }else{
+        user.friend.push(friendId)
+        friend.friends.push(friendId)
+    }
+
+    await user.save();
+    await friend.save()
+    const friends = await Promise.all(
+        user.friendList.map((id) => {
+          User.findById(id);
+        })
+      );
+      const formattedFriends = friends.map(
+        ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+          return { _id, firstName, lastName, occupation, location, picturePath };
+        }
+      );
+    res.status(200).json({formattedFriends})
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+    getUser,
+    getUserFriend,
+    addRemoveFriend,
+
+} from '../controllers/users.js';
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/*READ*/
+router.get("/:id",verifyToken,getUser);
+router.get("/:id/friends",getUserFriend);
+
+
+/*UPDATE*/
+router.patch("/:id/:friendId",addRemoveFriend)
+
+
+export default router;
+
